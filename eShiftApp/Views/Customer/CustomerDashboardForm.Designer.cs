d9a50ad8a4a5f28a7122dd10fbadaca64fc7336b
bavindushan <bavindushan65@gmail.com>
2025-07-16 23:06:49 +0530

    customerdashbord's dashbord ui done

---
+++ eShiftApp/Views/Customer/CustomerDashboardForm.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace eShiftApp.Views.Customer
+{
+    partial class CustomerDashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDashboard = new Guna.UI2.WinForms.Guna2Button();
+            this.btnAddNewRequest = new Guna.UI2.WinForms.Guna2Button();
+            this.btnSettings = new Guna.UI2.WinForms.Guna2Button();
+            this.btnLogout = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.progressTotalJobs = new Guna.UI2.WinForms.Guna2CircleProgressBar();
+            this.progressApprovedJobs = new Guna.UI2.WinForms.Guna2CircleProgressBar();
+            this.progressCompletedJobs = new Guna.UI2.WinForms.Guna2CircleProgressBar();
+            this.dataGridJobs = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.guna2Panel1.SuspendLayout();
+            this.guna2Panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridJobs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.guna2Panel1.Controls.Add(this.btnLogout);
+            this.guna2Panel1.Controls.Add(this.btnSettings);
+            this.guna2Panel1.Controls.Add(this.btnAddNewRequest);
+            this.guna2Panel1.Controls.Add(this.btnDashboard);
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(206, 516);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(468, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome Back!";
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnDashboard.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnDashboard.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnDashboard.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnDashboard.FillColor = System.Drawing.Color.RoyalBlue;
+            this.btnDashboard.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnDashboard.ForeColor = System.Drawing.Color.White;
+            this.btnDashboard.Location = new System.Drawing.Point(12, 28);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(180, 45);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "Dashboard";
+            // 
+            // btnAddNewRequest
+            // 
+            this.btnAddNewRequest.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddNewRequest.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddNewRequest.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnAddNewRequest.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnAddNewRequest.FillColor = System.Drawing.Color.RoyalBlue;
+            this.btnAddNewRequest.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnAddNewRequest.ForeColor = System.Drawing.Color.White;
+            this.btnAddNewRequest.Location = new System.Drawing.Point(13, 107);
+            this.btnAddNewRequest.Name = "btnAddNewRequest";
+            this.btnAddNewRequest.Size = new System.Drawing.Size(180, 45);
+            this.btnAddNewRequest.TabIndex = 1;
+            this.btnAddNewRequest.Text = "Add New Request";
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSettings.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnSettings.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnSettings.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnSettings.FillColor = System.Drawing.Color.RoyalBlue;
+            this.btnSettings.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnSettings.ForeColor = System.Drawing.Color.White;
+            this.btnSettings.Location = new System.Drawing.Point(13, 192);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(180, 45);
+            this.btnSettings.TabIndex = 2;
+            this.btnSettings.Text = "Settings";
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnLogout.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnLogout.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnLogout.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnLogout.FillColor = System.Drawing.Color.Red;
+            this.btnLogout.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Location = new System.Drawing.Point(11, 461);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(92, 45);
+            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Text = "Logout";
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.Controls.Add(this.progressCompletedJobs);
+            this.guna2Panel2.Controls.Add(this.progressApprovedJobs);
+            this.guna2Panel2.Controls.Add(this.progressTotalJobs);
+            this.guna2Panel2.Location = new System.Drawing.Point(221, 56);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(768, 112);
+            this.guna2Panel2.TabIndex = 1;
+            // 
+            // progressTotalJobs
+            // 
+            this.progressTotalJobs.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
+            this.progressTotalJobs.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.progressTotalJobs.ForeColor = System.Drawing.Color.White;
+            this.progressTotalJobs.Location = new System.Drawing.Point(63, 5);
+            this.progressTotalJobs.Minimum = 0;
+            this.progressTotalJobs.Name = "progressTotalJobs";
+            this.progressTotalJobs.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.progressTotalJobs.Size = new System.Drawing.Size(101, 101);
+            this.progressTotalJobs.TabIndex = 0;
+            this.progressTotalJobs.Text = "Total Jobs";
+            // 
+            // progressApprovedJobs
+            // 
+            this.progressApprovedJobs.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
+            this.progressApprovedJobs.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.progressApprovedJobs.ForeColor = System.Drawing.Color.White;
+            this.progressApprovedJobs.Location = new System.Drawing.Point(308, 4);
+            this.progressApprovedJobs.Minimum = 0;
+            this.progressApprovedJobs.Name = "progressApprovedJobs";
+            this.progressApprovedJobs.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.progressApprovedJobs.Size = new System.Drawing.Size(101, 101);
+            this.progressApprovedJobs.TabIndex = 1;
+            this.progressApprovedJobs.Text = "guna2CircleProgressBar2";
+            // 
+            // progressCompletedJobs
+            // 
+            this.progressCompletedJobs.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
+            this.progressCompletedJobs.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.progressCompletedJobs.ForeColor = System.Drawing.Color.White;
+            this.progressCompletedJobs.Location = new System.Drawing.Point(575, 4);
+            this.progressCompletedJobs.Minimum = 0;
+            this.progressCompletedJobs.Name = "progressCompletedJobs";
+            this.progressCompletedJobs.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.progressCompletedJobs.Size = new System.Drawing.Size(101, 101);
+            this.progressCompletedJobs.TabIndex = 2;
+            this.progressCompletedJobs.Text = "guna2CircleProgressBar3";
+            // 
+            // dataGridJobs
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dataGridJobs.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridJobs.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridJobs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridJobs.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridJobs.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridJobs.Location = new System.Drawing.Point(222, 175);
+            this.dataGridJobs.Name = "dataGridJobs";
+            this.dataGridJobs.RowHeadersVisible = false;
+            this.dataGridJobs.RowHeadersWidth = 62;
+            this.dataGridJobs.RowTemplate.Height = 28;
+            this.dataGridJobs.Size = new System.Drawing.Size(768, 330);
+            this.dataGridJobs.TabIndex = 2;
+            this.dataGridJobs.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridJobs.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dataGridJobs.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dataGridJobs.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dataGridJobs.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dataGridJobs.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridJobs.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridJobs.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.dataGridJobs.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridJobs.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridJobs.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dataGridJobs.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridJobs.ThemeStyle.HeaderStyle.Height = 4;
+            this.dataGridJobs.ThemeStyle.ReadOnly = false;
+            this.dataGridJobs.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dataGridJobs.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dataGridJobs.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridJobs.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dataGridJobs.ThemeStyle.RowsStyle.Height = 28;
+            this.dataGridJobs.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dataGridJobs.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+
+            // 
+            // CustomerDashboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 516);
+            this.Controls.Add(this.dataGridJobs);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2Panel2);
+            this.Controls.Add(this.guna2Panel1);
+            this.Name = "CustomerDashboardForm";
+            this.Text = "CustomerDashboardForm";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridJobs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Button btnLogout;
+        private Guna.UI2.WinForms.Guna2Button btnSettings;
+        private Guna.UI2.WinForms.Guna2Button btnAddNewRequest;
+        private Guna.UI2.WinForms.Guna2Button btnDashboard;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2CircleProgressBar progressTotalJobs;
+        private Guna.UI2.WinForms.Guna2CircleProgressBar progressCompletedJobs;
+        private Guna.UI2.WinForms.Guna2CircleProgressBar progressApprovedJobs;
+        private Guna.UI2.WinForms.Guna2DataGridView dataGridJobs;
+    }
+}
---
+++ eShiftApp/Views/Customer/CustomerDashboardForm.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Windows.Forms;
+using eShiftApp.Controllers;
+using eShiftApp.Models;
+using CustomerModel = eShiftApp.Models.Customer;
+
+namespace eShiftApp.Views.Customer
+{
+    public partial class CustomerDashboardForm : Form
+    {
+        private readonly CustomerModel _currentCustomer;
+        private readonly TransportJobController _jobController;
+
+        private int _targetTotal = 0;
+        private int _targetApproved = 0;
+        private int _targetCompleted = 0;
+
+        private Timer timerTotal;
+        private Timer timerApproved;
+        private Timer timerCompleted;
+
+
+        public CustomerDashboardForm(CustomerModel customer)
+        {
+            InitializeComponent();
+            _currentCustomer = customer;
+            _jobController = new TransportJobController();
+        }
+
+        private void CustomerDashboardForm_Load(object sender, EventArgs e)
+        {
+            // Create timers
+            timerTotal = new Timer { Interval = 20 };
+            timerTotal.Tick += TimerTotal_Tick;
+
+            timerApproved = new Timer { Interval = 20 };
+            timerApproved.Tick += TimerApproved_Tick;
+
+            timerCompleted = new Timer { Interval = 20 };
+            timerCompleted.Tick += TimerCompleted_Tick;
+
+
+            LoadJobStats();
+            LoadJobTable();
+        }
+
+        //  Load job counts into Guna2CircleProgressBars
+        private void LoadJobStats()
+        {
+            try
+            {
+                _targetTotal = _jobController.GetTotalJobCount(_currentCustomer.CustomerId);
+                _targetApproved = _jobController.GetJobCountByStatus(_currentCustomer.CustomerId, "Approved");
+                _targetCompleted = _jobController.GetJobCountByStatus(_currentCustomer.CustomerId, "Completed");
+
+                // Reset values to 0 before animation
+                progressTotalJobs.Value = 0;
+                progressApprovedJobs.Value = 0;
+                progressCompletedJobs.Value = 0;
+
+                progressTotalJobs.Text = "0";
+                progressApprovedJobs.Text = "0";
+                progressCompletedJobs.Text = "0";
+
+                // Start animations
+                timerTotal.Start();
+                timerApproved.Start();
+                timerCompleted.Start();
+
+                // Stop the timer immediately if target is 0
+                if (_targetTotal == 0) timerTotal.Stop();
+                if (_targetApproved == 0) timerApproved.Stop();
+                if (_targetCompleted == 0) timerCompleted.Stop();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading job stats: " + ex.Message);
+            }
+        }
+
+        private void TimerTotal_Tick(object sender, EventArgs e)
+        {
+            if (progressTotalJobs.Value < _targetTotal)
+            {
+                //progressTotalJobs.Value += 2;
+                progressTotalJobs.Value = Math.Min(progressTotalJobs.Value + 2, _targetTotal);
+                progressTotalJobs.Text = progressTotalJobs.Value.ToString();
+            }
+            else
+            {
+                timerTotal.Stop();
+            }
+        }
+
+        private void TimerApproved_Tick(object sender, EventArgs e)
+        {
+            if (progressApprovedJobs.Value < _targetApproved)
+            {
+                //progressApprovedJobs.Value += 2;
+                progressApprovedJobs.Value = Math.Min(progressApprovedJobs.Value + 2, _targetApproved);
+                progressApprovedJobs.Text = progressApprovedJobs.Value.ToString();
+            }
+            else
+            {
+                timerApproved.Stop();
+            }
+        }
+
+        private void TimerCompleted_Tick(object sender, EventArgs e)
+        {
+            if (progressCompletedJobs.Value < _targetCompleted)
+            {
+                //progressCompletedJobs.Value += 2;
+                progressCompletedJobs.Value = Math.Min(progressCompletedJobs.Value + 2, _targetCompleted);
+                progressCompletedJobs.Text = progressCompletedJobs.Value.ToString();
+            }
+            else
+            {
+                timerCompleted.Stop();
+            }
+        }
+
+        //  Load job list into DataGridView
+        private void LoadJobTable()
+        {
+            try
+            {
+                var jobs = _jobController.GetJobsByCustomerId(_currentCustomer.CustomerId);
+                dataGridJobs.DataSource = jobs;
+
+                // Optional styling
+                dataGridJobs.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dataGridJobs.ReadOnly = true;
+                dataGridJobs.AllowUserToAddRows = false;
+                dataGridJobs.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading job list: " + ex.Message);
+            }
+        }
+
+        //  Add New Request button from sidebar
+        private void btnAddNewRequest_Click(object sender, EventArgs e)
+        {
+            //var requestForm = new AddJobForm(_currentCustomer); // To be created next
+            //requestForm.ShowDialog();
+
+            LoadJobStats();
+            LoadJobTable();
+        }
+
+        //  Settings (placeholder)
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Settings page coming soon!");
+        }
+
+        //  Logout
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Close(); // You can also redirect to LoginForm
+        }
+
+        //  Reload Dashboard view
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            LoadJobStats();
+            LoadJobTable();
+        }
+    }
+}
